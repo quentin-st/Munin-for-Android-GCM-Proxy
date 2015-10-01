7396a77ea72395e84712602617594f2380fe6d97
Quentin Stoeckel <chteuchteu@gmail.com>
2015-10-01 14:42:58 +0200

    Simplify model

--- src/AppBundle/Service/GCMService.php
+++ src/AppBundle/Service/GCMService.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Model\Alert;
 use Endroid\Gcm\Client;
 
 class GCMService
@@ -14,8 +15,10 @@ class GCMService
         $this->gcmClient = $gcmClient;
     }
 
-    public function notifyAlert()
+    public function notifyAlert(array $regIds, Alert $alert)
     {
+        $data = $alert->toArray();
 
+        return $this->gcmClient->send($data, $regIds);
     }
 }
